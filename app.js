0977e2bfc0a416c8c451fb77f57cc64214460ef9
PhanHoangAnh <hoanganh17b@gmail.com>
2017-07-21 00:34:02 +0700

    restructure folders system

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./Vendors/globalComponents/routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+app.set('views', './Vendors/globalComponents/views');
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -23,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
